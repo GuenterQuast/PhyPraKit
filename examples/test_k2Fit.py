bb045b78ced1fe0532828601eb5121e7e6f9707e
Guenter Quast <guenter.quast@online.de>
2022-05-22 17:18:04 +0200

    completed complex kafe2go example; test_k2Fit equivalent to text_xyFit

--- examples/test_k2Fit.py
+++ examples/test_k2Fit.py
@@ -1,86 +1,92 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 """
 test_k2Fit
-   Illustrate fitting of an arbitrary function with kafe2  
-   This example illustrates the special features of kafe2:
-   - correlated errors for x and y data  
-   - relative errors with reference to model
-   - profile likelihood method to evaluate asymmetric errors
-   - plotting of profile likeliood and confidence contours
+   Illustrate fitting of an arbitrary function with kafe2   
+     This example illustrates the special features of kafe2:
+     - correlated errors for x and y data  
+     - relative errors with reference to model
+     - profile likelihood method to evaluate asymmetric errors
+     - plotting of profile likeliood and confidence contours
 
 .. moduleauthor:: Guenter Quast <g.quast@kit.edu>
 """
-
-from __future__ import print_function  # for python2.7 compatibility
 
 # import kafe2 # must be imported first to properly set matplotlib backend
 from PhyPraKit import generateXYdata, k2Fit
 import numpy as np, matplotlib.pyplot as plt
 
 # -- the model function
-def model(x, a=0.1, b=1., c=1.):
-   return a*x**2 + b*x + c
+#
+# Example of an application
+# 
+# model function to fit
+def expModel(x, A=1., x0=1.):
+  return A*np.exp(-x/x0)
 
 if __name__ == "__main__": # --------------------------------------  
 
-  # uncertainties 
-  sigy_abs = 0.1 # abs. independent errors on y
-  sigy_rel = 0.05 # relative error on y
-  syabscor=0.1  #  an absolute, correlated error on y
-  sigx_abs = 0.1 # absolute error on x
-  sigx_rel = 0.  # relative error on x
-  sxrelcor=0.02  #  a relative, correlated error on x 
+  model=expModel
+  mpardict = {'A':1., 'x0':1.}  # model parameters
 
-# generate the data
-  xmin =  1.
-  xmax =  10.
-  xdata=np.arange(xmin, xmax+1. ,1.)
-  nd=len(xdata)
-  mpars=[0.3, -1.5, 0.5]
-  np.random.seed(31415)
-  xt, yt, ydata = generateXYdata(xdata, model, sigx_abs, sigy_abs, 
-    srely=sigy_rel, xrelcor=sxrelcor, yabscor=syabscor, mpar=mpars )
+# set error components 
+  sabsy = 0.07
+  srely = 0.05 # 5% of model value
+  cabsy = 0.04
+  crely = 0.03 # 3% of model value
+  sabsx = 0.05
+  srelx = 0.04 # 4% 
+  cabsx = 0.03 
+  crelx = 0.02 # 2% 
 
-# fit with kafe2
-  parv, pare, cor, chi2 = k2Fit(model,
-    xdata, ydata,        # data and uncertainties
-    sx=sigx_abs,         # indep. x abs.
-    sy=sigy_abs,         # indep. y abs.
-    srelx=None,          # indep. x corr.
-    srely=sigy_rel,      # indep. y corr.
-    xabscor=None,        # correlated x abs.
-    xrelcor=sxrelcor,    # correlated x rel.
-    yabscor=syabscor,    # correlated y abs.
-    yrelcor=None,        # correlated y rel.
-    ref_to_model=True,   # reference of rel. uncert. to model
-    p0=(0.5,-1.,1.),     # initial guess for parameter values
-    dp0= None,           # initial guess for parameter uncertainties
-    plot=True,           # generate result plot (see options below)  
-    fit_info=True,       # suppress fit results in figure if False
-    plot_band=True,      # suppress model uncertainty-band if False
-    plot_cor=False,      # plot profiles and contours
-    showplots=True,     # plots on screen, plt.show() in user code if False
-    axis_labels=['x-data','random y'], # set nice names
-    data_legend = 'random data',       # legend entry for data points
-    model_name = r'f\,',               # name for model
-    model_expression = r'{a}\,{x}^2 + {b}\,{x} + {c}',  # model fuction
-    model_legend = 'quadratic model',  # legend entry for model line
-    model_band = None                  # name for model uncertainty band
-    )                      
+# ---> generate pseudo data
+  np.random.seed(314159)      # initialize random generator
+  nd=14
+  xmin=0.
+  xmax=2.6
+  data_x = np.linspace(xmin, xmax, nd)       # x of data points
+  sigy = np.sqrt(sabsy * sabsy + (srely*model(data_x, **mpardict))**2)
+  sigx = np.sqrt(sabsx * sabsx + (srelx * data_x)**2)
+  xt, yt, data_y = generateXYdata(data_x, model, sigx, sigy,
+                                      xabscor=cabsx,
+                                      xrelcor=crelx,
+                                      yabscor=cabsy,
+                                      yrelcor=crely,
+                                      mpar=mpardict.values() )
+# <---- end generate pseudo data
+
+# fit data with kafe2
+  par, pare, cor, chi2 = k2Fit(model,
+    data_x, data_y,
+    sabsx,
+    sabsy,                                    # data and uncertaintites
+    srelx=srelx,
+    srely=srely,
+    xabscor=cabsx,
+    xrelcor=crelx,
+    yabscor=cabsy,                            # correlated uncertainties
+    yrelcor=crely,
+    ref_to_model=True,
+    p0=(1., .5),                              # initial guess and range
+    quiet=True,
+    plot=True,                                # show plot, options below  
+    fit_info=True,                             # fit results in figure
+ #   plot_band=False,
+    plot_cor=True,
+    axis_labels=['x-data','random y'],        # nice names
+    data_legend = 'random data',              # legend entry for data points
+    model_name = r'f\,',                 # name for model
+    model_expression = r'{A}\,\exp({x}/{x0})',   # model fuction
+    model_legend = 'exponential model',       # legend entry for model line
+    #model_band = None,                       # name for model uncertainty band
+   )
 # setting any of the above names to None will remove the entry from the legend,
-#  if not specified, default is used  
-
-  print('*==* data set')
-  print('  x = ', xdata)
-  print('  y = ', ydata)
-  np.set_printoptions(precision=3)
-  print('  sx = ', np.sqrt(sigx_abs**2 + (sigx_rel*xdata)**2) )
-  print('  sy = ', np.sqrt(sigy_abs**2 + (sigy_rel*model(xdata, *parv))**2) )
+#  if not specified, use default  
 
   print('*==* fit result:')
   print("  -> chi2:         %.3g"%chi2)
   np.set_printoptions(precision=3)
-  print("  -> parameters:   ", parv)
+  print("  -> parameters:   ", par)
   np.set_printoptions(precision=2)
-  print("  -> uncertainties:\n", pare) 
+  print("  -> uncertainties:", pare) 
   print("  -> correlation matrix: \n", cor) 
+
