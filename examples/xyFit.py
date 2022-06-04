998b72331ac98e48978ba3877adabf60d1a28bb0
Guenter Quast <guenter.quast@online.de>
2022-06-04 10:15:13 +0200

    refactored code

--- examples/xyFit.py
+++ examples/xyFit.py
@@ -2,30 +2,148 @@
 # -*- coding: utf-8 -*-
 # script xyFit.py
 
-"""Perform fit with data and model from yaml file 
+def xyFit_from_file(fd,                 # dictionary definig fit input
+                    plot=True,          # plot data and model
+                    plot_band=True,     # plot model confidence-band
+                    plot_cor=False,     # plot profiles likelihood and contours
+                    quiet=True          # suppress informative printout
+           ):
 
-   Uses function PhyPraKit.phyfit.xyFit, a wrapper for phyFit.mnFit
+  """Perform fit with data and model from yaml file 
 
-   This code performs fits to x-y data with 
-   independent and correlated, absolute and relative 
-   uncertainties in the x and y directions. 
+    Uses function PhyPraKit.phyfit.xyFit, a wrapper for phyFit.mnFit
 
-   usage:
-     ./xyFit.py <file>
+    This code performs fits to x-y data with 
+    independent and correlated, absolute and relative 
+    uncertainties in the x and y directions. 
 
-  Input:
-    - file in yaml format
-   
-.. moduleauthor:: Guenter Quast <g.quast@kit.edu>
-"""
+    usage:
+      ./xyFit.py <file>
 
-# package imports
-import sys, yaml
-import numpy as np
-from PhyPraKit.phyFit import xyFit
+   Input:
+     - file in yaml format
+     - output options
+  
+  .. moduleauthor:: Guenter Quast <g.quast@kit.edu>
+  """
 
-# Helper functions
-def parse_code(code_string):
+  from PhyPraKit.phyFit import xyFit
+
+  def decode_uDict(uDict):
+    """Decode dictionary with uncertainties
+
+      yaml format:
+      y-errors:
+      - type:                     "simple" or "matrix"
+        error_value:              number or array
+        correlation_coefficient:  0. or 1.
+        relative:                 false or true
+
+      Args:
+        - uDict : dictionary with uncertainties
+     
+      Returns:  
+        - s       # independent errors
+        - srel    # independent relative errors
+        - sabscor # correlated absolute errors
+        - srelcor # correlated relative errors
+    """
+  
+    def decode_rel(e):
+      """Decode numbers with %-sign: interpreted as relative errors
+      """
+      if type(e) is type(''):
+        if '%' in e:
+          v = float(e[0:e.find('%')])/100.
+      else:
+          v = e
+      return v
+
+    s = None        # independent errors
+    srel = None     # independent relative errors
+    sabscor = None  # correlated absolute errors
+    srelcor = None  # correlated relative errors
+
+    if type(uDict) is not type([]):      # got a scalar, one error for all
+      if type(uDict) is type(''):
+        if '%' in uDict:
+          srel=decode_rel(uDict)
+      else:
+        s = uDict
+      return s, srel, sabscor, srelcor
+
+    if type(uDict[0]) is not type({}): # array of uncertainties, one for each data point
+      for i, v in enumerate(uDict):
+        if type(v) is type(''):
+          uDict[i]=decode_rel(v) 
+          srel= uDict
+        else:                          
+          s = uDict
+      return s, srel, sabscor, srelcor
+        
+    # else decode complex error dictionary
+    for ed in uDict:
+      if 'type' in ed:
+        typ = ed['type']
+      else:
+        typ = "simple"
+        if 'relative' in ed:
+          rel = ed['relative']
+        else:
+          rel = False   
+      if 'correlation_coefficient' in ed:
+        cor = ed['correlation_coefficient'] 
+      else:
+        cor = 0.
+      if 'error_value' in ed:
+        e = ed['error_value']
+        if type(e) is type([]):
+          for i,v in enumerate(e):
+            if type(v) is type(''):  
+              if '%' in v:
+                rel = True
+                e[i]=decode_rel(v)
+        else:
+          if type(e) is type(''):  
+            if '%' in e:
+              rel = True
+              e=decode_rel(e)          
+      else:
+        e = None
+     #    
+      if cor == 0. and not rel:
+        # independent absolute error
+        if s is None:
+          s = e
+        else:
+          s = [s]
+          s.append(e)
+      elif cor == 0. and rel:
+        # independent relative error
+        if srel is None:
+          srel = e
+        else:
+          srel = [srel]
+          srel.append(e)
+      elif cor !=0 and not rel:
+        # correlated absolute error
+        if sabscor is None:
+          sabscor = e
+        else:
+          sabscor = [sabscor]
+          sabscor.append(e)
+      elif cor !=0 and rel:
+        # correlated relative error
+        if srelcor is None:
+          srelcor = e
+        else:
+          srelcor = [srelcor]
+          srelcor.append(e)
+     # -- end for 
+    return s, srel, sabscor, srelcor  
+
+  # Helper functions
+  def parse_code(code_string):
     """Watch out for "dangerous" commands in Python code and extract function name
 
       Args: 
@@ -58,152 +176,9 @@ def parse_code(code_string):
         sys.exit(1)
     return function_name, code_string 
 
-                
-def decode_uDict(uDict):
-  """Decode dictionary with uncertainties
+# --- end helper functions 
 
-    yaml format:
-    y-errors:
-    - type:                     "simple" or "matrix"
-      error_value:              number or array
-      correlation_coefficient:  0. or 1.
-      relative:                 false or true
-
-    Args:
-      - uDict : dictionary with uncertainties
-     
-    Returns:  
-      - s       # independent errors
-      - srel    # independent relative errors
-      - sabscor # correlated absolute errors
-      - srelcor # correlated relative errors
-  """
-  
-  def decode_rel(e):
-    """Decode numbers with %-sign: interpreted as relative errors
-    """
-    if type(e) is type(''):
-      if '%' in e:
-        v = float(e[0:e.find('%')])/100.
-    else:
-        v = e
-    return v
-
-  s = None        # independent errors
-  srel = None     # independent relative errors
-  sabscor = None  # correlated absolute errors
-  srelcor = None  # correlated relative errors
-
-  if type(uDict) is not type([]):      # got a scalar, one error for all
-    if type(uDict) is type(''):
-      if '%' in uDict:
-        srel=decode_rel(uDict)
-    else:
-      s = uDict
-    return s, srel, sabscor, srelcor
-
-  if type(uDict[0]) is not type({}): # got an array of uncertainties
-    for i, v in enumerate(uDict):
-      if type(v) is type(''):
-        uDict[i]=decode_rel(v) 
-        srel= uDict
-      else:                          
-        s = uDict
-    return s, srel, sabscor, srelcor
-        
-  # decode complex error dictionary
-  for ed in uDict:
-    if 'type' in ed:
-      typ = ed['type']
-    else:
-      typ = "simple"
-      if 'relative' in ed:
-        rel = ed['relative']
-      else:
-        rel = False   
-    if 'correlation_coefficient' in ed:
-      cor = ed['correlation_coefficient'] 
-    else:
-      cor = 0.
-    if 'error_value' in ed:
-      e = ed['error_value']
-      if type(e) is type([]):
-        for i,v in enumerate(e):
-          if type(v) is type(''):  
-            if '%' in v:
-              rel = True
-              e[i]=decode_rel(v)
-      else:
-        if type(e) is type(''):  
-          if '%' in e:
-            rel = True
-            e=decode_rel(e)          
-    else:
-      e = None
-    #    
-    if cor == 0. and not rel:
-      # independent absolute error
-        if s is None:
-          s = e
-        else:
-          s = [s]
-          s.append(e)
-    elif cor == 0. and rel:
-      # independent relative error
-        if srel is None:
-          srel = e
-        else:
-          srel = [srel]
-          srel.append(e)
-    elif cor !=0 and not rel:
-      # correlated absolute error
-        if sabscor is None:
-          sabscor = e
-        else:
-          sabscor = [sabscor]
-          sabscor.append(e)
-    elif cor !=0 and rel:
-      # correlated relative error
-        if srelcor is None:
-          srelcor = e
-        else:
-          srelcor = [srelcor]
-          srelcor.append(e)
-   # -- end for 
-  return s, srel, sabscor, srelcor  
-
-if __name__ == "__main__": # --------------------------------------  
-  #
-  # Example of an application of PhyPraKit.xyFit()
-  #
-
-  # load file
-# check for / read command line arguments
-  if len(sys.argv) == 2:
-    filen = sys.argv[1]
-  else:
-    print("!!! no file name given")
-    sys.exit(1)
-
-# open and read input yaml file    
-  try:
-    with open(filen) as f:
-      fd = yaml.load(f, Loader=yaml.Loader)
-  except (OSError, yam.YAMLError) as exception:
-    print('!!! failed to read configuration file ' + filen)
-    print(str(exception))
-    sys.exit(1)
-      
-  # another check
-  if len(fd.keys()) == 0:
-    print("!!! data file is empty!")
-    sys.exit(1)
-
-  print("*==*", sys.argv[0], "received valid yaml data for fit:")
-  # pprint.pprint(fd)
-
-
-# extract contents   
+  # Extract information from input dictionary   
   if 'label' in fd:
     data_label = fd['label']
   else:
@@ -294,8 +269,50 @@ if __name__ == "__main__": # --------------------------------------
       plot_cor=False,     # plot profiles likelihood and contours
       quiet=True          # suppress informative printout
       )
+  return rdict
+                 
 
-# print results to illustrate how to use output
+if __name__ == "__main__": # --------------------------------------  
+  #
+  # xyFit.py: Example of an application of PhyPraKit.phyFit.xyFit_from_file()
+  #
+
+  # package imports
+  import sys, yaml, numpy as np
+  
+  # load file
+  # check for / read command line arguments
+  if len(sys.argv) == 2:
+    filen = sys.argv[1]
+  else:
+    print("!!! no file name given")
+    sys.exit(1)
+
+  # open and read input yaml file    
+  try:
+    with open(filen) as f:
+      fd = yaml.load(f, Loader=yaml.Loader)
+  except (OSError, yam.YAMLError) as exception:
+    print('!!! failed to read configuration file ' + filen)
+    print(str(exception))
+    sys.exit(1)
+      
+  # another check
+  if len(fd.keys()) == 0:
+    print("!!! data file is empty!")
+    sys.exit(1)
+
+  print("*==*", sys.argv[0], "received valid yaml data for fit:")
+  # pprint.pprint(fd)
+
+  rdict = xyFit_from_file(fd,                 # the input dictionary defining the fit 
+                 plot=True,          # plot data and model
+                 plot_band=True,     # plot model confidence-band
+                 plot_cor=False,     # plot profiles likelihood and contours
+                 quiet=True          # suppress informative printout
+                 ) 
+
+  # print results to illustrate how to use output
   print('\n*==* Fit Result:')
 
   pvals, perrs, cor, chi2, pnams= rdict.values()
@@ -307,6 +324,6 @@ if __name__ == "__main__": # --------------------------------------
   print(" pos. parameter errors: ", perrs[:,1])
   print(" correlation matrix : \n", cor)
 
-#- alternative: print result dictionary
-#  for key in rdict:
-#    print("{}\n".format(key), rdict[key])
+  #- alternative: print result dictionary
+  #  for key in rdict:
+  #    print("{}\n".format(key), rdict[key])
