d36deeefd7c19d952edac0b49f10d2cba2f77e8b
Guenter Quast <guenter.quast@online.de>
2022-06-21 19:49:12 +0200

    added arg parser

--- tools/csv2yml.py
+++ tools/csv2yml.py
@@ -1,37 +1,92 @@
 #!/usr/bin/env python3
 """csv2yml.py 
-  uses csv2yaml to read floating-point column-data in very general .txt 
-  formats and writes an output block in yaml format
+  read floating-point column-data in very general .txt  formats 
+  and write an output block in yaml format
+
+  keys taken from 1st header line
+
+  Usage:
+
+    ./cvs2yml [options] <input file name>
+
+  Input: 
+
+    - file name 
+
+  Output:
+
+    - yaml data block 
+  
 
 .. moduleauthor:: Guenter Quast <g.quast@kit.edu>
 
 """
-from PhyPraKit import csv2yaml
 
-if __name__ == "__main__": # -------------------------------------------  
-  import sys, PhyPraKit as ppk, yaml
+if __name__ == "__main__": # -------------------------------------------
 
-  # check for / read command line arguments
-  if len(sys.argv)==2:
-    fnam = sys.argv[1]
-  else:
-    print("!!! no input file given - exiting")
-    sys.exit(1)
+  import sys, yaml, argparse
+  from PhyPraKit import csv2yaml
+
+  # - - - Parse command-line arguments
+  # parser = argparse.ArgumentParser(usage=__doc__)
+  parser = argparse.ArgumentParser(usage="cvs2yml [options] <input file name>")
+
+  parser.add_argument('filename', type=str,
+      help="name of fit input file in csv format")
+  parser.add_argument('-s', '--save', 
+      action='store_const', const=True, default=False,
+      help="save result in file")
+  parser.add_argument('-H', '--Header', 
+      action='store_const', const=True, default=False,
+      help="print csv Header lines")
+  parser.add_argument('-q', '--quiet', 
+      action='store_const', const=True, default=False,
+      help="quiet - no output to screen")
+  parser.add_argument('-d','--delimiter', 
+                      type=str, default=',',
+           help="delimiter, default=','")
+  parser.add_argument('-n','--header_lines', 
+                      type=int, default=1,
+           help="numer of header lines, default=1")
   
-  # read data from file in .txt format
-  fnam = sys.argv[1]
-  nlhead = 1
-  delim = ','
-  f = open(fnam, 'r') 
+  if len(sys.argv)==1:  # print help message if no input given
+    parser.print_help()
+    sys.exit(1)
 
+  # collect input from ArgumentParser
+  args = parser.parse_args()
+  fnam = args.filename
+
+  nlhead = args.header_lines
+  delim = args.delimiter
+  showHeader = args.Header
+  sav_flg = args.save
+  quiet = args.quiet
+  if quiet: sav_flg = True
+
+  # ---- end argument parsing -------------------
+  
+  f = open(fnam, 'r') 
   hlines, ylines = csv2yaml(f, nlhead, delim)
 
-  # print to screen
-  for l in ylines:
-    print(l)
+  if not quiet:
+    print('-->', fnam, 'to yaml', end='')
+
+  if showHeader:
+    print('  csv header:')
+    for l in hlines:
+      print(l)
+
+  # print results to screen
+  if not quiet:
+    print('  yaml block:')
+    for l in ylines:
+      print(l)
 
   # write to file
-  with open(fnam.split('.')[0] + '.yaml', 'w') as f:
-   for l in ylines:
-     print(l, file=f)
-
+  if sav_flg:
+    ymlfn = fnam.split('.')[0] + '.yaml'
+    with open(ymlfn, 'w') as f:
+     for l in ylines:
+       print(l, file=f)
+    print('   -> yaml saved in file',ymlfn) 
