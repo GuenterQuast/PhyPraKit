d845c40b7b2af274570db7231894cd0889464fbb
Guenter Quast <guenter.quast@kit.edu>
2023-04-11 18:24:31 +0200

    version 1.2.5 SS23

--- PhyPraKit/_version_info.py
+++ PhyPraKit/_version_info.py
@@ -1,14 +1,14 @@
 '''
 .. module:: _version_info
    :platform: >=3.6
-   :synopsis: Version 1.2.3 of PhyPraKit, rel. Sept. 2022
+   :synopsis: Version 1.2.5 of PhyPraKit, rel. Apr. 2023
 
 .. moduleauthor:: Guenter Quast <g.quast@kit.edu>
 '''
 
 major = 1
 minor = 2
-revision = 4
+revision = 5
 
 def _get_version_tuple():
   '''
