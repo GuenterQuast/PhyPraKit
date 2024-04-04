68acc731917d2a4c8d680b2d2d3a4748df54f199
GuenterQuast <Guenter.Quast@online.de>
2024-04-04 14:47:31 +0200

    version 1.2.7 for summer term 2024

--- PhyPraKit/_version_info.py
+++ PhyPraKit/_version_info.py
@@ -1,14 +1,14 @@
 """
 .. module:: _version_info
    :platform: >=3.6
-   :synopsis: Version 1.2.5 of PhyPraKit, rel. Apr. 2023
+   :synopsis: Version 1.2.7 of PhyPraKit, rel. Apr. 2024
 
 .. moduleauthor:: Guenter Quast <g.quast@kit.edu>
 """
 
 major = 1
 minor = 2
-revision = 6
+revision = 7
 
 
 def _get_version_tuple():
